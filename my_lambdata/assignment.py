50e49497e9933a677f7ccff8456c529ea19529aa
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-07 23:35:56 -0400

    Document via docstrings

--- my_lambdata/assignment.py
+++ my_lambdata/assignment.py
@@ -2,6 +2,15 @@
 from pandas import DataFrame
 
 def add_state_names(my_df):
+    """
+    Adds a column of state names to accompany a corresponding column of state abbreviation.
+
+    Params:
+        my_df (pandas.DataFrame) has a column called "abbrev" with state abbreviations
+
+    Returns:
+        copy of the original dataframe, with another column
+    """
     new_df = my_df.copy()
     names_map = {"CA":"Cali", "CO":"Colo", "CT":"Conn"}
     new_df["name"] = new_df["abbrev"].map(names_map) # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
